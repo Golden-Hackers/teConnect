16fb4cd2ee068f293a388ae831d9d07013d08829
Piczle <44884494+Piczle@users.noreply.github.com>
2019-10-06 06:55:59 -0400

    Add files via upload

---
+++ Golden Hack/client/app.js
@@ -0,0 +1,16 @@
+(function()  {
+
+  //Initialize firebase
+  const config= {
+
+    apiKey: "AIzaSyB5yFt6gS_xe__qszfTbYsbOljIUVSv7N0",
+    authDomain: "web-quikstart-5505a.firebaseapp.com",
+    databaseURL: "https://web-quickstart-5505a.firebaseio.com",
+    storageBucket: "web-quickstart-5505a.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+
+
+
+}());
